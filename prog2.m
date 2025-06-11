2b2611c2bc8447d7e29a7415c0c1e5bc1bb650ea
Nirmala <125744640+Nimpari@users.noreply.github.com>
2025-06-11 11:52:55 +0530

    Update prog1.1.m
    % Display color Image, find its complement and convert to gray scale

--- prog2.m
+++ prog2.m
@@ -1,4 +1,17 @@
- %Simulation of an Image( Arithmetic & Logic Operation) 
+
+1.1 % Display color Image, find its complement and convert to gray scale
+ 
+
+I=imread('cancercell.jpg'); 
+subplot(2,2,1); imshow(I); subimage(I); title('Color Image'); 
+c=imcomplement(I); 
+subplot(2,2,2); imshow(c); subimage(c); title('Complement of color Image'); 
+r=rgb2gray(I); 
+subplot(2,2,3); imshow(r); subimage(r); title('Gray scale of color Image'); 
+%Complement of Gray Image 
+b=imcomplement(r); 
+subplot(2,2,4); imshow(b); subimage(b); title('Complement of Gray Image'); 
+%Simulation of an Image( Arithmetic & Logic Operation) 
 a=ones(40); b=zeros(40); 
 c=[a b;b a]; d=[b b;a a]; 
 A=10*(c+d); 
